1d590db994d019a0b879907a9429b1862d22617a
sedat <sedatcyalcin@gmail.com>
2021-02-28 00:42:29 +0300

    add token amount and token price

--- yield_explorer.py
+++ yield_explorer.py
@@ -3,6 +3,7 @@ import os
 import sys
 from database import db, cursor
 
+
 def main():
     wallet_address = os.getenv('WALLET_ADDRESS')
     provider = os.getenv('PROVIDER')
@@ -44,7 +45,6 @@ def main():
 
     wallet_balance = response['result']['walletBalance']['totalUSDValue']
 
-
     def get_staked_tokens(staked_tokens, staked_vaults):
         print("Deposit: " + "{:.2f}USD".format(staked_tokens['deposit']) + ", Yield: " + "{:.2f}USD".format(
             staked_tokens['yield']) + ", Total: " + "{:.2f}USD".format(staked_tokens['total']))
@@ -57,11 +57,10 @@ def main():
                   "{:.2f}".format(stake['depositedTokens'] * stake['priceInUSDDepositToken']))
             print("Total yield: " + "{:.2f}".format(stake['totalRewards']))
             print("********")
-            sql = "INSERT INTO yield_history (pool, deposit, yield) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (stake['depositToken'], (stake['depositedTokens']
-                                                         * stake['priceInUSDDepositToken']), stake['totalRewards']))
+            sql = "INSERT INTO yield_history (pool, token_amount, token_price, deposit, yield) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (stake['depositToken'], stake['depositedTokens'], stake['priceInUSDDepositToken'], (stake['depositedTokens']
+                                                                                                                    * stake['priceInUSDDepositToken']), stake['totalRewards']))
             db.commit()
-
 
     def get_lp_tokens(lp_staked, lp_staked_vaults):
         print("Deposit: " + "{:.2f}USD".format(lp_staked['deposit']) + ", Yield: " + "{:.2f}USD".format(
@@ -70,17 +69,18 @@ def main():
             print("{:.2f}".format(
                 stake['LPInfo']['currentToken0'] * stake['LPInfo']['priceInUSDToken0'] + stake['LPInfo']['currentToken1'] * stake['LPInfo']['priceInUSDToken1']) + "USD - " + stake['name'] + " deposited")
             print("Total yield: " + "{:.2f}".format(stake['totalRewards']))
-            sql = "INSERT INTO lp_yield_history (pool, deposit, yield) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (stake['name'], (stake['LPInfo']['currentToken0'] * stake['LPInfo']['priceInUSDToken0'] +
-                                                 stake['LPInfo']['currentToken1'] * stake['LPInfo']['priceInUSDToken1']), stake['totalRewards']))
+            sql = "INSERT INTO lp_yield_history (pool, first_token_amount, second_token_amount, first_token_price, second_token_price, deposit, yield) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (stake['name'], stake['LPInfo']['currentToken0'], stake['LPInfo']['currentToken1'], stake['LPInfo']['priceInUSDToken0'], stake['LPInfo']['priceInUSDToken1'], (stake['LPInfo']['currentToken0'] * stake['LPInfo']['priceInUSDToken0'] +
+                                                                                                                                                                                               stake['LPInfo']['currentToken1'] * stake['LPInfo']['priceInUSDToken1']), stake['totalRewards']))
             db.commit()
 
-
     print("Initial deposit: " + str(deposit))
+    print("Current value: " + str(wallet_balance))
     print("\nToken Stake")
     get_staked_tokens(staked, staked_vaults)
     print("\nLP Stake")
     get_lp_tokens(lp_staked, lp_staked_vaults)
     print("\nTotal amount: " +
           "{:.2f}".format(lp_staked['total'] + staked['total']))
-    print("Total yield: " + "{:.2f}".format(lp_staked['yield'] + staked['yield']))
+    print("Total yield: " +
+          "{:.2f}".format(lp_staked['yield'] + staked['yield']))
